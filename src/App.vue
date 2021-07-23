c00d9f0a042f9f9b08049420b956b4bba9f60a2d
Cory Francis Myers <cfm@panix.com>
2021-07-22 22:13:39 -0700

    fix: don't use (i.e., expose) OAuth client secret

--- src/App.vue
+++ src/App.vue
@@ -88,7 +88,6 @@ export default {
   async mounted() {
     jsforce.browser.init({
       clientId: process.env.VUE_APP_SALESFORCE_CLIENT_ID,
-      clientSecret: process.env.VUE_APP_SALESFORCE_CLIENT_SECRET,
       loginUrl: process.env.VUE_APP_SALESFORCE_LOGIN_URL,
       redirectUri: process.env.VUE_APP_SALESFORCE_REDIRECT_URI,
     });
