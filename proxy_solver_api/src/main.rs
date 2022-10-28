f9b32e76dabfc90ca7a529cc674003504dca3c03
Cory Francis Myers <cfm@panix.com>
2022-10-27 17:36:38 -0700

    feat(/solution): count members and members present

--- proxy_solver_api/src/main.rs
+++ proxy_solver_api/src/main.rs
@@ -1,15 +1,32 @@
 #[macro_use]
 extern crate rocket;
 use rocket::http::Status;
+use rocket::serde::json::{json, Json, Value};
+use rocket::serde::Deserialize;
 
 #[get("/health/ready")]
 fn health_ready() -> Status {
     Status::NoContent
 }
 
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct AttendanceSnapshot {
+    members: Vec<String>,
+    members_present: Vec<String>,
+}
+
+#[post("/solution", data = "<snapshot>")]
+fn solution(snapshot: Json<AttendanceSnapshot>) -> Value {
+    json!({
+        "members": snapshot.members.len(),
+        "members_present": snapshot.members_present.len(),
+    })
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![health_ready])
+    rocket::build().mount("/", routes![health_ready, solution])
 }
 
 #[cfg(test)]
