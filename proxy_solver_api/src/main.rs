0d7bf042cce54bd8fb194160ad4ed544c4a3a13f
Cory Francis Myers <cfm@panix.com>
2022-10-27 17:14:46 -0700

    feat(/health/ready): reimplement health-check endpoint in Rust/Rocket

--- proxy_solver_api/src/main.rs
+++ proxy_solver_api/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+use rocket::http::Status;
+
+#[get("/health/ready")]
+fn health_ready() -> Status {
+    Status::NoContent
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![health_ready])
 }
