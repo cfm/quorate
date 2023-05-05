80b0a63b2d445c5d3f530cfbe93c1f3759fd271a
Cory Francis Myers <cfm@panix.com>
2023-05-04 17:55:23 -0700

    feat(/solution): use sortable Index{Map,Set} and seeded RNG for reproducibility

--- proxy_solver_api/src/main.rs
+++ proxy_solver_api/src/main.rs
@@ -2,13 +2,14 @@
 extern crate matchmaker;
 #[macro_use]
 extern crate rocket;
+use indexmap::map::IndexMap;
+use indexmap::set::IndexSet;
 use matchmaker::da_stb::match_students;
 use matchmaker::{Category, Student};
-use rand::thread_rng;
+use rand::{rngs::StdRng, SeedableRng};
 use rocket::http::Status;
 use rocket::serde::json::{json, Json, Value};
 use rocket::serde::Deserialize;
-use std::collections::{HashMap, HashSet};
 
 #[get("/health/ready")]
 fn health_ready() -> Status {
@@ -22,11 +23,11 @@ struct MemberInfo {
 }
 
 trait Member {
-    fn from_info(info: &MemberInfo, present: &HashMap<String, Category>) -> Student;
+    fn from_info(info: &MemberInfo, present: &IndexMap<String, Category>) -> Student;
 }
 
 impl Member for Student {
-    fn from_info(info: &MemberInfo, present: &HashMap<String, Category>) -> Student {
+    fn from_info(info: &MemberInfo, present: &IndexMap<String, Category>) -> Student {
         Student {
             name: info.id.clone(),
             preferences: info
@@ -54,14 +55,14 @@ struct AttendanceSnapshot {
 
 #[post("/solution", data = "<snapshot>")]
 fn solution(snapshot: Json<AttendanceSnapshot>) -> Value {
-    let mut presents: HashMap<String, Category> = HashMap::new();
+    let mut presents: IndexMap<String, Category> = IndexMap::new();
     for id in &snapshot.members_present {
         let present = Category::new(&id, 2); // FIXME: constant
         presents.insert(id.clone(), present);
     }
 
-    let mut absents: HashMap<String, Student> = HashMap::new();
-    let mut unrepresented: HashSet<String> = HashSet::new();
+    let mut absents: IndexMap<String, Student> = IndexMap::new();
+    let mut unrepresented: IndexSet<String> = IndexSet::new();
     for info in &snapshot.members {
         if !presents.contains_key(&info.id) {
             let absent = <Student as Member>::from_info(info, &presents);
@@ -73,14 +74,14 @@ fn solution(snapshot: Json<AttendanceSnapshot>) -> Value {
     println! {"absent={} members={:?}", absents.len(), absents.keys()}
     println! {"present={} members={:?}", presents.len(), presents.keys()}
 
-    let mut rng = thread_rng();
+    let mut rng = StdRng::seed_from_u64(0);
     let result = match_students(
         absents.clone().into_values().collect(),
         &Vec::from(presents.clone().into_values().collect::<Vec<_>>()),
         &mut rng,
     );
 
-    let mut proxies: HashMap<String, String> = HashMap::new();
+    let mut proxies: IndexMap<String, String> = IndexMap::new();
     for present in presents.values() {
         for absent in result.placed.get(&present.name).unwrap_or(&Vec::new()) {
             unrepresented.remove(&absent.name);
@@ -88,6 +89,9 @@ fn solution(snapshot: Json<AttendanceSnapshot>) -> Value {
             println!("{} → {}", &present.name, &absent.name);
         }
     }
+
+    proxies.sort_keys();
+    unrepresented.sort();
 
     json!({
         "represented": proxies,
