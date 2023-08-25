afbe4935057ccf7427c8e18d498d69a0c7afa105
Cory Francis Myers <cfm@panix.com>
2023-08-24 19:05:12 -0700

    build: Webpack 5 compatibility, but revert to Vue/Vuetify 2
    Vue/Vuetify 3 compatibility, apparently a challenging upgrade[1], is
    beyond the scope of this round of housekeeping.
    
    [1]: https://www.reddit.com/r/vuetifyjs/comments/126zkzb/vuevuetify_2_to_vuevuetify_3_migration_is/

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,13 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
 process.env.VUE_APP_URL =
   process.env.CONTEXT == 'production'
     ? process.env.URL
     : process.env.DEPLOY_PRIME_URL;
 
 module.exports = {
+  chainWebpack: (config) => {
+    config.plugin('polyfills').use(NodePolyfillPlugin);
+  },
   transpileDependencies: ['vuetify'],
 };
